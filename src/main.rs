da19a7fcb07186655a3f49e55de3628732ec72ba
delbyte <blazed0utcode@gmail.com>
2025-02-18 22:21:28 +0530

    removed mutability where it was not needed

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,23 @@ use std::io;
 fn main() {
     println!("Hello, world!");
     
-    let mut arr = [1,2,3,4];
+    let arr = [1,2,3,4];
     
-    let mut s = String::from("Hello World");
+    let s = String::from("Hello World");
 
     for i in arr{
         println!("The element is {i}")
     }
 
-    for i in (1..10) {
+    for i in 1..10 {
         println!("TLDR{i}")
     }
 
-    fn calculate_length(s: String) -> usize {
+    fn calculate_length(s: &String) -> usize {
         return s.len();
     }
-    let mut name = String::from("name");
-    println!("{calculate_length(name)}");
+    let name = String::from("name");
+    println!("{}", calculate_length(&name));
 
 }
 
