d9ca1126cc9acc98bbbb62f97bf62987179ae51a
delbyte <blazed0utcode@gmail.com>
2025-02-24 18:06:43 +0530

    marked done stuff as done

--- src/main.rs
+++ src/main.rs
@@ -121,9 +121,9 @@ fn display_tasks() {
 
 //Pseudo Code Planning 
 
-// 1. I need a list of tasks from the user, empty initially. It will be a list of lists of strings. When the user inputs a task, [pending] gets automatically assigned to it.
-// 2. I need a function to remove the [pending] from the list element and add [complete] instead. 
-// 3. I need a function to remove the [pending] from the list element and add [working] instead.
+// DONE 1. I need a list of tasks from the user, empty initially. It will be a list of lists of strings. When the user inputs a task, [pending] gets automatically assigned to it.
+// DONE 2. I need a function to remove the [pending] from the list element and add [complete] instead. 
+// DONE 3. I need a function to remove the [pending] from the list element and add [working] instead.
 // 4. I need to have some sort of clock to remove the [complete] tasks from the list the next day, and maybe some sort of reminder. 
 // 5. I WANT A FRONTEND WAAAA I WANT A WAY TO STORE THE TASKS IN A DATABASE WAAAAAAAAA 
 
