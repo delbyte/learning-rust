03d5b3d58fe57bfdf73f7b60cf747c90cc7c925b
delbyte <blazed0utcode@gmail.com>
2025-02-26 13:26:41 +0530

    added stuf

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
+    // Read the request
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    let response = "HTTP/1.1 200 OK\r\n\rHello world!";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            // Send the response
+            let response = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
+            match stream.write(response.as_bytes()) {
+                Ok(_) => {
+                    stream.flush().unwrap();
+                }
+                Err(e) => {
+                    eprintln!("Failed to write to stream: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to read from stream: {}", e);
+        }
+    }
 }
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(|| {
+                thread::spawn(move || {
                     handle_client(stream);
                 });
             }
             Err(e) => {
-                println!("Error: {}", e);
+                eprintln!("Error: {}", e);
             }
         }
     }
